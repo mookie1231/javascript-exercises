cd54f1c7a2cf5cbc460902d6c97847c222270050
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-02-25 18:30:45 -0500

    More practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
 const repeatString = function() {
-
+    for(x = 0; x < 3; x++){
+        console.log("Hello World") // replace hello world with function input
+    }
 };
 
 // Do not edit below this line
